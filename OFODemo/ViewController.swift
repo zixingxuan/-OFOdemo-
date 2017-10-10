536b2201544b49824274932f495b3bf11d1827df
DJAPpple_4 <zhangweidong@cpsol.cn>
2017-10-10 16:39:25 +0800

    Initial Commit

---
+++ OFODemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OFODemo
+//
+//  Created by DJAPpple_4 on 2017/10/10.
+//  Copyright © 2017年 DJAPpple_4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
